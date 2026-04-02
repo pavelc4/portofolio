9ad2ba65cb65465d0d08484a7dadeb15d1c717d8
Pavelc4 <pavelc4@users.noreply.github.com>
2026-04-02 20:01:42 +0700

    chore (style)  : import Initial css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import compressor from '@playform/compress';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://pavelc4.github.io', 
+  site: 'https://pavelc4.github.io',
   integrations: [
     svelte(),
-    tailwind({ applyBaseStyles: false }),
     sitemap(),
-    compressor(),
   ],
-  image: {
-    remotePatterns: [{ protocol: 'https', hostname: 'github.com' }],
+  vite: {
+    plugins: [tailwindcss()],
   },
   compressHTML: true,
   build: {
