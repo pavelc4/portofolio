1d34011960da416a137d7371ae20e917494bc2c5
Pavelc4 <pavelc4@users.noreply.github.com>
2026-04-02 19:45:59 +0700

    Chore initial project scafold

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import compressor from '@playform/compress';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://pavelc4.github.io', 
+  integrations: [
+    svelte(),
+    tailwind({ applyBaseStyles: false }),
+    sitemap(),
+    compressor(),
+  ],
+  image: {
+    remotePatterns: [{ protocol: 'https', hostname: 'github.com' }],
+  },
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
+});
