7518cb3bae2d229d3f9001ededb507efa68ce096
pavelc4 <dev@pavelc4.com>
2026-04-03 05:53:15 +0700

    chore: integrate astro-icon and install iconify datasets

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: 'https://pavelc4.github.io',
-  integrations: [
-    svelte(),
-    sitemap(),
-  ],
+  integrations: [svelte(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
