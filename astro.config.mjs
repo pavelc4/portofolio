c313715233aaafb5eead9afd802cfda218c8238e
Pavelc4 <pavelc4@users.noreply.github.com>
2026-08-03 23:47:07 +0700

    fix(config): change Astro site base path from /portofolio to /

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://pavelc4.github.io',
-  base: '/portofolio',
+  base: '/',
   integrations: [svelte(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
